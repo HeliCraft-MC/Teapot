b1ce67b6a8d156bef78f5f997d3fa9ddccf21a6e
ms0ur <maksimsuslov7142@gmail.com>
2025-06-20 21:05:07 +0300

    added config for pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'HeliCraft-Teapot',
+            exec_mode: 'cluster',
+            instances: '2',
+            script: './.output/server/index.mjs',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3300
+            }
+        }
+    ]
+}
