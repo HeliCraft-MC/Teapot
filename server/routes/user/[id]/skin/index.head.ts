ed3fa1bc559a591edd0c6ddad91127c3ec354523
ms0ur <maksimsuslov7142@gmail.com>
2025-06-23 18:44:38 +0300

    maybe this

--- server/routes/user/[id]/skin/index.head.ts
+++ server/routes/user/[id]/skin/index.head.ts
@@ -24,28 +24,9 @@ defineRouteMeta({
 export default defineEventHandler(async (event) => {
     const id = getRouterParam(event, 'id')
     const uuid = await resolveUuid(id)
-
-    const meta = getSkin(uuid)
-    const { uploadDir = './uploads' } = useRuntimeConfig()
-    let buf: Buffer
-    let mime: string
-
-    if (meta) {
-        try {
-            buf = await fsp.readFile(join(uploadDir, meta.path))
-            mime = meta.mime
-        } catch (err: any) {
-            if (err.code !== 'ENOENT') throw err
-        }
-    }
-
-    if (!buf) {
-        buf = await fsp.readFile('defaultSkin.png')
-        mime = 'image/png'
-    }
-
-    event.node.res.setHeader('Content-Length', buf.length.toString())
+    event.node.res.setHeader('Content-Length', 2000)
     event.node.res.setHeader('Cache-Control', 'no-store, must-revalidate');
+    event.node.res.setHeader('Content-Type', 'image/png')
 
 
 
