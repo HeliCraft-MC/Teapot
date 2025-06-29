553584976d60dafe4b13e03aa11ed6b368ec9826
Maksim <73832732+ms0ur@users.noreply.github.com>
2025-06-29 21:42:36 +0300

    Still hate head requests

--- server/routes/user/[id]/skin/index.head.ts
+++ server/routes/user/[id]/skin/index.head.ts
@@ -24,12 +24,32 @@ defineRouteMeta({
 export default defineEventHandler(async (event) => {
     const id = getRouterParam(event, 'id')
     const uuid = await resolveUuid(id)
-    event.node.res.setHeader('Content-Length', 2000)
+
+    const meta = getSkin(uuid)
+    const { uploadDir = './uploads' } = useRuntimeConfig()
+    let buf: Buffer
+    let mime: string
+
+    if (meta) {
+        try {
+            buf = await fsp.readFile(join(uploadDir, meta.path))
+            mime = meta.mime
+        } catch (err: any) {
+            if (err.code !== 'ENOENT') throw err
+        }
+    }
+
+    if (!buf) {
+        buf = await fsp.readFile('defaultSkin.png')
+        mime = 'image/png'
+    }
+
+    event.node.res.setHeader('Content-Length', buf.length.toString())
     event.node.res.setHeader('Cache-Control', 'no-store, must-revalidate');
     event.node.res.setHeader('Content-Type', 'image/png')
 
 
 
 
-    return ''
+    return send(event, buf, mime)
 })
