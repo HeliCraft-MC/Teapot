7b9398fc88d4b8748c700711f035b506ef4325a2
Ktilis <ktilis.dev@gmail.com>
2025-06-29 05:35:56 +0500

    docker run fix9

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 import path from 'path';
 
 dotenv.config({ path: path.join(__dirname, '.env') });
-console.log(__dirname, process.env.MYSQL_USER);
 
 export default defineNitroConfig({
     srcDir: "server",
--- server/plugins/mySql.ts
+++ server/plugins/mySql.ts
@@ -36,6 +36,7 @@ export default defineNitroPlugin((nitro) => {
             });
 
             console.log("config[connection].options.user: "+config[connection].options.user);
+            console.log(__dirname, process.env.MYSQL_USER);
 
             console.log(`[MySQL Plugin] Connection ${connection} initialized.`);
         } catch (err) {
