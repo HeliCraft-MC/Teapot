460ff1958fb7a0f91e26e652c5fe199abd1df0e2
Ktilis <ktilis.dev@gmail.com>
2025-06-29 05:17:11 +0500

    docker run fix5

--- server/plugins/mySql.ts
+++ server/plugins/mySql.ts
@@ -35,6 +35,8 @@ export default defineNitroPlugin((nitro) => {
                 keepAliveInitialDelay: 0,
             });
 
+            console.log("config[connection].options.user: "+config[connection].options.user);
+
             console.log(`[MySQL Plugin] Connection ${connection} initialized.`);
         } catch (err) {
             console.error(`[MySQL Plugin] Error initializing connection ${connection}:`, err);
