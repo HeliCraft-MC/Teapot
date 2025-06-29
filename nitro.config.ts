23a330bb4349d7e79f0cc382cfa5f864896e226a
Ktilis <ktilis.dev@gmail.com>
2025-06-29 06:12:21 +0500

    Revert "docker run fix12"
    This reverts commit 8fd6c30a0f85b8b2c72c52cf3c86755aec816b90.

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,10 @@
+//https://nitro.unjs.io/config
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 export default defineNitroConfig({
-    srcDir: 'server',
+    srcDir: "server",
     experimental: {
         asyncContext: true,
         database: false,
@@ -8,41 +13,40 @@ export default defineNitroConfig({
     openAPI: {
         ui: {
             scalar: {
-                theme: 'purple',
-            },
+                theme: 'purple'
+            }
         },
         meta: {
             title: 'HeliCraft Backend API',
             description: 'Backend API for HeliCraft project',
-            version: '1.0',
-        },
+            version: '1.0'
+        }
     },
     runtimeConfig: {
-        // Git commit подставится в runtime, если прокинуть через NODE_COMMIT
-        teapotCommit: '',
-        jwtSecret: '',
-        uploads: '',
-        sqliteSkinPath: '',
-        databaseDebug: false,
+        teapotCommit: process.env.NODE_COMMIT || 'unknown',
+        jwtSecret: process.env.JWT_SECRET,
+        uploads: process.env.UPLOAD_DIR,
+        sqliteSkinPath: process.env.SQLITE_PATH,
+        databaseDebug: Boolean(process.env.DATABASE_DEBUG) || false,
         database: {
             default: {
                 options: {
-                    host: '',
-                    port: 0,
-                    user: '',
-                    password: '',
-                    database: '',
-                },
+                    host:     process.env.MYSQL_HOST,
+                    port:     Number(process.env.MYSQL_PORT),
+                    user:     process.env.MYSQL_USER,
+                    password: process.env.MYSQL_PASSWORD,
+                    database: process.env.MYSQL_DATABASE,
+                }
             },
             states: {
                 options: {
-                    host: '',
-                    port: 0,
-                    user: '',
-                    password: '',
-                    database: '',
-                },
-            },
+                    host:     process.env.STATES_MYSQL_HOST,
+                    port:     Number(process.env.STATES_MYSQL_PORT),
+                    user:     process.env.STATES_MYSQL_USER,
+                    password: process.env.STATES_MYSQL_PASSWORD,
+                    database: process.env.STATES_MYSQL_DATABASE,
+                }
+            }
         },
-    },
-})
+    }
+});
