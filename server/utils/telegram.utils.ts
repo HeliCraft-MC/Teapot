eab4ca82ae2a9019a718c57cadc624a0f0bad650
elisartix <egorobunov@yandex.ru>
2025-07-01 20:02:04 +0300

    фикс2

--- server/utils/telegram.utils.ts
+++ server/utils/telegram.utils.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { $fetch } from 'ofetch';
-import FormData from 'form-data';
+import * as FormData from 'form-data';
 import { Buffer } from 'node:buffer';
 import { useRuntimeConfig } from '#imports';
 
