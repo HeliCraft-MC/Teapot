483557332cff18f338c3c55afc3e29b3ea88ae5f
ms0ur <maksimsuslov7142@gmail.com>
2025-12-24 12:36:16 +0300

    add support for topics

--- nitro.config.ts
+++ nitro.config.ts
@@ -34,22 +34,24 @@ export default defineNitroConfig({
         databaseDebug: Boolean(process.env.DATABASE_DEBUG) || false,
         telegramBotToken: process.env.TELEGRAM_BOT_TOKEN,
         telegramChatId: process.env.TELEGRAM_CHAT_ID,
+        telegramThreadId: process.env.TELEGRAM_THREAD_ID,
+        telegramTopicSkins: process.env.TELEGRAM_TOPIC_SKINS,
         publicApiUrl: process.env.PUBLIC_API_URL,
         database: {
             default: {
                 options: {
-                    host:     process.env.MYSQL_HOST,
-                    port:     Number(process.env.MYSQL_PORT),
-                    user:     process.env.MYSQL_USER,
+                    host: process.env.MYSQL_HOST,
+                    port: Number(process.env.MYSQL_PORT),
+                    user: process.env.MYSQL_USER,
                     password: process.env.MYSQL_PASSWORD,
                     database: process.env.MYSQL_DATABASE,
                 }
             },
             states: {
                 options: {
-                    host:     process.env.STATES_MYSQL_HOST,
-                    port:     Number(process.env.STATES_MYSQL_PORT),
-                    user:     process.env.STATES_MYSQL_USER,
+                    host: process.env.STATES_MYSQL_HOST,
+                    port: Number(process.env.STATES_MYSQL_PORT),
+                    user: process.env.STATES_MYSQL_USER,
                     password: process.env.STATES_MYSQL_PASSWORD,
                     database: process.env.STATES_MYSQL_DATABASE,
                 }
